a2b25b3dabf47ed3c7f0755d0a344c2ef1bb330b
Charlie B <charles@bachmeier.io>
2023-07-10 06:16:24 -0700

    add new files

---
+++ config/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app'
+import {
+  FIREBASE_KEY,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: FIREBASE_KEY,
+  authDomain: 'ditch-prototype.firebaseapp.com',
+  databaseURL: 'https://ditch-prototype.firebaseio.com',
+  projectId: 'ditch-prototype',
+  storageBucket: 'ditch-prototype.appspot.com',
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = initializeApp(firebaseConfig)
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
