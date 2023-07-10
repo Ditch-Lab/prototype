686716fe1815654327b462379ce161817fe84cfe
Charlie B <charles@bachmeier.io>
2023-07-09 22:00:27 -0700

    working firebase config

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,8 @@ declare module '@env' {
   export const WEB3_AUTH_CLIENT_ID: string
   export const RPC_URL: string
   export const PAYMASTER_URL: string
+  export const FIREBASE_KEY: string
+  export const FIREBASE_MESSAGING_SENDER_ID: string
+  export const FIREBASE_APP_ID: string
+  export const FIREBASE_MEASUREMENT_ID: string
 }
