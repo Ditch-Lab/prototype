fffe765c4192d025174cdb16f8c3c7b8f02b357c
Charles Bachmeier <charlie@genie.xyz>
2023-07-20 08:20:14 -0700

    import ethers and get chaindId from magic provider

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -5,18 +5,28 @@ import { magicClient } from '../config/magicLink'
 import { magicUserAtom } from '../utils/hooks/useMagicUser'
 import { useAtom } from 'jotai'
 
+import RPC from '../config/ethers' // for using ethers.js
+
 export default function HomeScreen () {
   const [user, setUser] = useAtom(magicUserAtom)
+  const [chainId, setChainId] = React.useState('')
   async function handleLogout () {
     await magicClient.user.logout()
     setUser(undefined)
+  }
+  async function getChainId () {
+    const chainId = await RPC.getChainId()
+    const stringChainId = JSON.stringify(chainId)
+    setChainId(stringChainId)
   }
   return (
     <View style={styles.container}>
       <Text>Welcome {user?.email}!</Text>
       <Text>Wallet Address:{'\n'} {user?.publicAddress}</Text>
+      <Text>Chain Id:{'\n'} {chainId}</Text>
 
       <Button title="Sign Out" style={styles.button} onPress={handleLogout} />
+      <Button title="Get Chain Id" style={styles.button} onPress={getChainId} />
     </View>
   )
 }
